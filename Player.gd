71c8147d785de7d4482568740c07ef5781b9e64a
corecaps <corecaps@gmail.com>
2024-03-24 21:32:03 +0100

    updated player controls

--- Player.gd
+++ Player.gd
@@ -1,26 +1,29 @@
 extends Area2D
-
+# external scenes
 @export var bullet_scene : PackedScene
+@export var ghost_scene : PackedScene
+# movement params 
 @export var playerSpeed = 200
 @export var playerRotSpeed = 270
+# dash mechanism 
 @export var dashSpeed = 1000
 var isDashing = false
-var dashDuration = 0.2
-var dashCoolDown = 0.5
+var dashDuration = 0.05
+var dashCoolDown = 0.8
 var timeSinceLastDash = 0.0
-
+# ghost anim
+var draw_ghost = 0
+# shoot mechanism 
 var canShoot = true
-var shootCoolDown = 0.1
-# Called when the node enters the scene tree for the first time.
+var shootCoolDown = 0.05
+
 func _ready():
 	set_process(true)
 
 func start(pos):
 	position = pos
 	$bulletCoolDown.wait_time = shootCoolDown
-	
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	$Sprite2D.frame = ($Sprite2D.frame + 1) % 5
 	if timeSinceLastDash < dashCoolDown:
@@ -31,12 +34,13 @@ func _process(delta):
 	input = input.rotated(rotation)
 	if Input.is_action_just_pressed("dash"):
 		start_dash()
-	var currentSpeed = playerSpeed
-	if isDashing:
-		currentSpeed = dashSpeed
-		dashDuration -= delta
-		if dashDuration <= 0 :
-			stop_dash()
+	var currentSpeed = calc_speed(delta)
+	if (input.x != 0 || input.y != 0):
+		if (draw_ghost == 5):
+			create_ghost()
+			draw_ghost = 0
+		else :
+			draw_ghost += 1 
 	position += input * currentSpeed * delta
 	var rot = 0
 	if (Input.is_action_pressed("left")):
@@ -44,15 +48,21 @@ func _process(delta):
 	if (Input.is_action_pressed("right")):
 		rot += playerRotSpeed
 	rotation += deg_to_rad(rot * delta)
-	var screen_size = get_viewport_rect().size
-	if position.x < 0:
-		position.x += screen_size.x
-	elif position.x > screen_size.x:
-		position.x -= screen_size.x
-	if position.y < 0:
-		position.y += screen_size.y
-	elif position.y > screen_size.y:
-		position.y -= screen_size.y
+
+func create_ghost():
+	var g = ghost_scene.instantiate()
+	g.position = self.position
+	g.rotation = self.rotation
+	get_tree().root.add_child(g)
+
+func calc_speed(delta):
+	if not isDashing:
+		return playerSpeed
+	var currentSpeed = dashSpeed
+	dashDuration -= delta
+	if dashDuration <= 0 :
+		stop_dash()
+	return currentSpeed
 
 func start_dash():
 	isDashing = true
@@ -74,6 +84,7 @@ func shoot():
 	bullet.rotation = self.rotation + deg_to_rad(-90)
 	get_tree().root.add_child(bullet)
 
-
 func _on_bullet_cool_down_timeout():
 	canShoot = true
+
+
