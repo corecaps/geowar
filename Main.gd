d48bfcc165f8b26ecaec2bb66b75ef13e9588db0
corecaps <corecaps@gmail.com>
2024-03-24 21:30:28 +0100

    Added quit button HUD display

--- Main.gd
+++ Main.gd
@@ -2,12 +2,19 @@ extends Node2D
 @export var PlayerScene : PackedScene
 var Player : Area2D
 @onready var screensize = get_viewport_rect().size
-# Called when the node enters the scene tree for the first time.
+
+
 func _ready():
 	Player = PlayerScene.instantiate()
 	get_tree().root.add_child.call_deferred(Player)
 	Player.start(Vector2(screensize.x / 2,screensize.y / 2))
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
+
+func _process(_delta):
+	$CanvasLayer/hudPositionLabel.text = "Player Position x :" + str(Player.position.x) + " y: " + str(Player.position.y)
+
+func _on_button_pressed():
+	get_tree().quit()
+	
+func getPlayerPos():
+	return Player.position
